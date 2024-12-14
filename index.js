94d5e4abb6a312b35762bcda0ba36a9d55ca6479
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-12-14 17:51:00 +0600

    post job and get all job

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb')
+require('dotenv').config()
+
+const port = process.env.PORT || 9000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@tariqul-islam.mchvj.mongodb.net/?retryWrites=true&w=majority&appName=TARIQUL-ISLAM`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+
+async function run() {
+  try {
+    const jobsCollection = client.db('SoloSphere_DB').collection('Jobs')
+
+    // post a new job
+    app.post('/post-job', async (req, res) => {
+      const newJob = req.body
+      const result = await jobsCollection.insertOne(newJob)
+      res.send(result)
+    })
+
+    //get all jobs
+    app.get('/jobs', async (req, res) => {
+      const result = await jobsCollection.find().toArray()
+      res.send(result)
+    })
+
+    // get my posted jobs
+    app.get('/my-posted-jobs/:email', async (req, res) => {
+      const email = req.params.email
+      const query = { 'buyer.email': email }
+      const result = await jobsCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      '☘️  You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('Hello from SoloSphere Server....')
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
