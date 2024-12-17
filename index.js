d45e03201450ea385f2d20466f138a4186650d11
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-12-17 11:32:10 +0600

    genarat jwt token and verify token

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser');
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const port = process.env.PORT || 9000
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}))
 app.use(express.json())
+app.use(cookieParser())
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.SoloSphere_Token
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+
+  //verify the token
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+
+    req.user = decoded
+    next()
+  })
+};
 
 const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@tariqul-islam.mchvj.mongodb.net/?retryWrites=true&w=majority&appName=TARIQUL-ISLAM`;
 
@@ -22,6 +45,28 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const jobsCollection = client.db('SoloSphere_DB').collection('Jobs')
+
+    // Create a jwt token
+    app.post('/jwt', (req, res) => {
+      try {
+        const userInfo = req.body
+        const token = jwt.sign(userInfo, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+        res.cookie('SoloSphere_Token', token, {
+          httpOnly: true,
+          secure: false
+        }).send({ success: true })
+      } catch (error) {
+        return res.status(500).send({ message: error.code })
+      }
+    })
+
+    //logout when not access jwt token
+    app.post('/logout', (req, res) => {
+      res.clearCookie('SoloSphere_Token', {
+        httpOnly: true,
+        secure: false
+      }).send({ success: true })
+    })
 
     // post a new job
     app.post('/post-job', async (req, res) => {
@@ -45,9 +90,14 @@ async function run() {
     })
 
     // get my posted jobs
-    app.get('/my-posted-jobs/:email', async (req, res) => {
-      const email = req.params.email
+    app.get('/my-posted-jobs', verifyToken, async (req, res) => {
+      const { email } = req.query
       const query = { 'buyer.email': email }
+
+      if (req?.user?.email !== email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
       const result = await jobsCollection.find(query).toArray()
       res.send(result)
     })
